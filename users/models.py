b9bd06b5b0b3acf006390acc609a1f275a737c7b
D1 <sesepro@gmail.com>
2022-11-02 22:13:50 +0100

    Adding custom users handling module CRUD

---
+++ users/models.py
@@ -0,0 +1,18 @@
+from django.db import models
+from django.contrib.auth.models import AbstractUser
+from django.utils.translation import gettext_lazy as _
+from .managers import CustomUserManager
+
+class CustomUser(AbstractUser):
+    username = None
+    email = models.EmailField(_('email address'), unique=True)
+    # first_name = models.CharField(_('first name'), max_length=250)
+    # last_name = models.CharField(_('last name'), max_length=250)
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = []
+
+    objects = CustomUserManager()
+
+    def __str__(self) -> str:
+        return self.email
