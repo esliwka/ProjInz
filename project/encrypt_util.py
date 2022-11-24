0d504aec3dcde2c10521837ca715b1b91ea38e23
D1 <sesepro@gmail.com>
2022-11-24 01:02:25 +0100

    Server side hashing added

--- project/encrypt_util.py
+++ project/encrypt_util.py
@@ -1,8 +1,12 @@
 import base64
+import pickle
+import hashlib
 import logging
 import traceback
 from django.conf import settings
 from cryptography.fernet import Fernet
+from cryptography.hazmat.primitives import hashes
+from cryptography.hazmat.backends import default_backend
 
 def encrypt_data(data_):
     try:        
@@ -27,3 +31,19 @@ def decrypt_data(data_):
     except Exception as e:
         logging.getLogger("error_logger").error(traceback.format_exc())
         return None
+
+def hash_data(data_):
+    H = hashes.Hash(hashes.SHA3_224(), backend=default_backend())
+    H.update(pickle.dumps(data_))
+    return H.finalize()
+
+def sec_hash(data_, hash_type=""):
+    if hash_type == 'sha3_2':
+        return hashlib.sha3_256(data_.encode()).hexdigest()
+    elif hash_type == 'sha3_3':
+        return hashlib.sha3_384(data_.encode()).hexdigest()
+    elif hash_type == 'sha3_5':
+        return hashlib.sha3_512(data_.encode()).hexdigest()
+    else:
+        return hashlib.sha3_224(str(data_).encode()).hexdigest()
+    
--- users/views.py
+++ users/views.py
@@ -3,11 +3,20 @@ from django.contrib.auth import get_user_model
 import random
 from project.encrypt_util import *
 
+# User = get_user_model()
+# users = User.objects.all()
+# my_random_user = random.choice(users)
+# encrypted = encrypt_data(my_random_user)
+# decrypted = decrypt_data(encrypted)
+
+# def users_view(request):
+#     return render(request, 'users.html', {'user': (encrypted , my_random_user, decrypted )})
+
 User = get_user_model()
 users = User.objects.all()
 my_random_user = random.choice(users)
-encrypted = encrypt_data(my_random_user)
-decrypted = decrypt_data(encrypted)
+hashed_one = sec_hash(my_random_user)
+hashed_two = sec_hash(my_random_user)
 
 def users_view(request):
-    return render(request, 'users.html', {'user': (encrypted , my_random_user, decrypted )})
+    return render(request, 'users.html', {'user': (hashed_one , my_random_user, hashed_two , hashed_one == hashed_two)})
