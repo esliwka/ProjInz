3ab68ede84361d0dab233683d3f973488ab44752
esliwka <e_sliwka@tuta.io>
2022-12-10 19:39:57 +0100

    made models for final solution

--- polls/models.py
+++ polls/models.py
@@ -1,12 +1,13 @@
 from django.db import models
-from project.settings import AUTH_USER_MODEL
+#from project.settings import AUTH_USER_MODEL
 
 
 class Users(models.Model):
     first_name = models.CharField(max_length=30)
     last_name = models.CharField(max_length=30)
-    email = models.CharField(max_length=50)
+    email = models.CharField(max_length=100)
     password = models.CharField(max_length=100)
+    role = models.CharField(max_length=50)
 
     def __str__(self):
         return self.email
@@ -23,15 +24,24 @@ class TokenPolls(models.Model):
 class Polls(models.Model):
     poll_name = models.CharField(max_length=250)
     poll_text = models.TextField()
+    poll_owner = models.OneToOneRel(Users, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.poll_name
+
+
+class PollRespondents(models.Model):
+    user_id = models.ForeignKey(Users, on_delete=models.CASCADE)
+    poll_id = models.ForeignKey(Polls, on_delete=models.CASCADE)
 
     def __str__(self):
         return self.poll_name
 
 
 class UserPollStatus(models.Model):
-    user_id = models.ForeignKey(UserData, on_delete=models.CASCADE)
+    user_id = models.ForeignKey(Users, on_delete=models.CASCADE)
     poll_id = models.ForeignKey(Polls, on_delete=models.CASCADE)
-    answered = models.Bool()
+    answered = models.BooleanField()
 
     def __str__(self):
         return self.answered
@@ -63,7 +73,7 @@ class OpenQuestions(models.Model):
         return self.question_text
 
 
-class ClosedAnswers(models.Model):
+class OpenAnswers(models.Model):
     question_id = models.ForeignKey(ClosedQuestions, on_delete=models.CASCADE)
     answer = models.TextField()
 
