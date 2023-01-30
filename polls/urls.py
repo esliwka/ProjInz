198c48ede52e6f5c8112ad63c57184558cb51533
DF <sesepro@gmail.com>
2023-01-30 19:36:12 +0100

    Odswiezenie requirements

--- polls/urls.py
+++ polls/urls.py
@@ -9,7 +9,7 @@ urlpatterns = [
     path('logout/', views.logout_view, name='logout'),
     path('user_home/', views.user_home, name='user_home'),
     path('change_password/', views.change_password, name='change_password'),
-    path('redis-test/', views.redis_test, name='redis_test'),
+    
     path('create_poll/', views.create_poll, name='create_poll'),
     path('poll-list/', views.poll_list, name='poll_list'),
     path('add-respondent/', views.add_respondent, name='add_respondent'),
